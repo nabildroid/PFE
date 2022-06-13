3c412a06598f13a8ca4c3f580ea490a84828b434
nabil <pni20156789@gmail.com>
2022-06-13 00:03:22 +0000

    dynamic routes

--- backend/routes/admin/formation/inscription.js
+++ backend/routes/admin/formation/inscription.js
@@ -1,28 +1,37 @@
 import express from "express";
+import { getGroups } from "../../../models/group";
+import { getInscription } from "../../../models/inscription";
+import { getSalles } from "../../../models/salles";
+import { getTeachers } from "../../../models/teachers";
 const router = express.Router();
-
-
 
 // get insciption
 router.get("/:inId", (req, res) => {
-    res.send("inscription page");
+    const { inId, id } = req.params; // todo id of the formation is not working
+    const inscription = getInscription(inId);
+    const groups = getGroups(id);
+    const salles = getSalles();
+    const teachers = getTeachers();
+
+    res.render("demande", {
+        inscription,
+        groups,
+        salles,
+        teachers,
+    });
 });
 
 // delete insciption
-router.delete("/:inId", (req, res) => {
+router.delete("/:inId", (req, res) => {});
 
+// set insciption group
+router.post("/:inId", (req, res) => {
+    const { group } = req.body;
 });
 
 // set insciption group
 router.post("/:inId", (req, res) => {
-    const {group} = req.body;
+    const { group } = req.body;
 });
-
-// set insciption group
-router.post("/:inId", (req, res) => {
-    const {group} = req.body;
-});
-
-
 
 export default router;
