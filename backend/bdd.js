c092653c56ab5b7e25397b78cb59f89439586c0a
nabildroid <pni20156789@gmail.com>
2022-06-14 18:41:28 +0100

    connect to db from env

--- backend/bdd.js
+++ backend/bdd.js
@@ -2,11 +2,10 @@ import mysql from "mysql";
 
 
 const DB =mysql.createConnection({
-    host: "qfhi95794bta.eu-west-2.psdb.cloud",
-    user: "sq64s70r5n56",
-    password: "pscale_pw_rX63dMWbVJF06DnjpA-DG8lWehSHmHt7zZwV5Moj_jQ",
-    database: "cerist",
-    ssl:true,
+    host: process.env.DB_HOST ?? "localhost",
+    user: process.env.DB_USER ??"admin",
+    password: process.env.DB_PASSWORD ??"admin",
+    database: process.env.DB_NAME ??"cerist",
 });
 
 DB.connect(function (err) {
