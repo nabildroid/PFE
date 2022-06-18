317c784bef2601994cc0ae8832114618818b049f
nabildroid <pni20156789@gmail.com>
2022-06-18 22:22:47 +0100

    alow temporary all users

--- backend/routes/admin/index.js
+++ backend/routes/admin/index.js
@@ -9,16 +9,17 @@ import { getAdminForamtions } from "../../models/formation";
 
 // ensures the use is authenticated
 router.use((req, res, next) => {
-    if (req.session.user) {
-        next();
-    } else res.redirect("/login");
+  req.session.user = 1;
+  if (req.session.user) {
+    next();
+  } else res.redirect("/login");
 });
 
 /* GET users listing. */
-router.get("/",async (req, res) => {
-    const formations =await getAdminForamtions();
+router.get("/", async (req, res) => {
+  const formations = await getAdminForamtions();
 
-    res.render("admin", { formations });
+  res.render("admin", { formations });
 });
 
 router.use("/salles", salles);
