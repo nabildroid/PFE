3de3951eb49ca083a8d1ca180efa2c27163732ac
nabildroid <pni20156789@gmail.com>
2022-06-22 10:02:03 +0100

    fix typos

--- backend/routes/inscription.js
+++ backend/routes/inscription.js
@@ -5,25 +5,27 @@ import { getFormation } from "../models/formation";
 import { createInscription } from "../models/inscription";
 
 /* GET home page. */
-router.get("/:id",  async (req, res) =>{
-    const id = req.params.id;
-    const formation = await getFormation(id);
+router.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  if (id == "favicon.ico") return res.send("icon");
 
-    res.render("inscription", {
-        formation,
-        title: "Formation " + formation.title,
-    });
+  const formation = await getFormation(id);
+
+  res.render("inscription", {
+    formation,
+    title: "Formation " + formation.title,
+  });
 });
 
 router.post("/:id", (req, res) => {
-    const { name, fonction, organisme, email, tel } = req.body;
-    const { id } = req.params;
+  const { name, fonction, organisme, email, tel } = req.body;
+  const { id } = req.params;
 
-    createInscription(id, name, fonction, organisme, email, tel);
-    res.redirect("/");
+  createInscription(id, name, fonction, organisme, email, tel);
+  res.redirect("/");
 
-    // https://tailwindui.com/components/application-ui/overlays/modals
-    // redirect in local
+  // https://tailwindui.com/components/application-ui/overlays/modals
+  // redirect in local
 });
 
 export default router;
